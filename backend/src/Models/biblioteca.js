77c8d75b70b00c98a54aac316df32b8bfea6aea5
Jefry <matajefry2@gmail.com>
2025-10-27 19:24:24 -0600

    Estilo y correciones de route de biblioteca

--- backend/src/Models/biblioteca.js
+++ backend/src/Models/biblioteca.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const libroSchema = new mongoose.Schema({
   titulo: { type: String, required: true },
   autor: { type: String, required: true },
   categoria: { type: String },
   disponible: { type: Boolean, default: true },
-  fechaRegistro: { type: Date, default: Date.now }
+  archivoUrl: { type: String, required: true }, // ✅ se agrega
+  nombreArchivo: { type: String, required: true }, // ✅ se agrega para eliminar luego
+  fechaCreacion: { type: Date, default: Date.now },
 });
 
-const Libro = mongoose.model('libro', libroSchema, "libros");
+const Libro = mongoose.model("Libro", libroSchema, "libros");
 module.exports = Libro;
