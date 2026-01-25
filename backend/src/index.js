8a08f431a5b057bdf9745e097dcbd9520745632a
befc04 <beferrera@unah.hn>
2026-01-25 11:28:19 -0600

    N

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://siam-production-1c5d.up.railway.app"
+  "https://siam-production-cce4.up.railway.app"
 ];
 
 app.use(cors({
