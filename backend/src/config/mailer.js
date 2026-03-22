5b3fa17173f4fdb273ccc5d8e75c807b97aebdf8
befc04 <beferrera@unah.hn>
2026-03-21 23:30:17 -0600

    a

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -1,11 +1,16 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host:   'smtp.gmail.com',
+  port:   465,
+  secure: true,
   auth: {
     user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS  // contraseña de aplicación, NO tu contraseña normal
-  }
+    pass: process.env.GMAIL_PASS
+  },
+  connectionTimeout: 10000,
+  greetingTimeout:   10000,
+  socketTimeout:     15000,
 });
 
 module.exports = transporter;
